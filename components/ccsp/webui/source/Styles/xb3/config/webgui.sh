acf9254465feb27d38f604cbd3f74fb63fb8ed94
zhangyt <2326925852@qq.com>
2024-12-03 17:21:03 +0800

    xb3

--- components/ccsp/webui/source/Styles/xb3/config/webgui.sh
+++ components/ccsp/webui/source/Styles/xb3/config/webgui.sh
@@ -16,7 +16,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-##########################################################################
+##########################################################################  //zhangyt
 
 #WEBGUI_SRC=/fss/gw/usr/www/html.tar.bz2
 #WEBGUI_DEST=/var/www
